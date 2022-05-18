32104cf5c85b182a8bab2b60f9bb62ab4a5cf73a
heonnn <najh0094@gamil.com>
2022-05-18 15:53:41 +0900

    Helloworld@Dog 객체 bark 호출

--- hello-git/src/com/kh/git/Helloworld.java
+++ hello-git/src/com/kh/git/Helloworld.java
@@ -1,9 +1,14 @@
 package com.kh.git;
 
+import com.kh.animal.Dog;
+
 public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		Dog dog = new Dog();
+		dog.bark();
 	}
 
 }
