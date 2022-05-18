7b6b030c5a80f16df4e8d5295568b9b07eb35ee6
heonnn <najh0094@gamil.com>
2022-05-18 16:03:18 +0900

    Cat class 추가
    Hellowolrd@Cat 객체 cry 호출

--- hello-git/src/com/kh/git/Helloworld.java
+++ hello-git/src/com/kh/git/Helloworld.java
@@ -1,5 +1,6 @@
 package com.kh.git;
 
+import com.kh.animal.Cat;
 import com.kh.animal.Dog;
 
 public class Helloworld {
@@ -9,6 +10,9 @@ public class Helloworld {
 		
 		Dog dog = new Dog();
 		dog.bark();
+		
+		Cat cat = new Cat();
+		cat.cry();
 	}
 
 }
